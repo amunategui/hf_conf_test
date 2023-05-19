c8e7e55caac0870896f19de7fba4a7c735fb51f9
Manuel Amunategui <amunategui@gmail.com>
2023-05-19 22:34:01 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+# uvicorn main:app --host 0.0.0.0 --port 10000
+from pydantic import BaseModel
+# import pinecone
+from fastapi import FastAPI
+from typing import Optional
+import os, json
+ 
+
+app = FastAPI()
+
+
+# Define a Pydantic model that will be used to validate the incoming JSON payload
+class HF_Convo_Item(BaseModel):
+    prompt: str
+    chat_history: list
+    script_name: str
+    page_number: int
+        
+@app.post("/conversation/")
+async def read_item(payload: HF_Convo_Item):
+  
+     
+    # Access individual fields of the JSON payload
+    prompt = payload.prompt
+    print(prompt)
+    
+    chat_history = payload.chat_history
+    print(chat_history)
+      
+    script = payload.script
+    print(script)
+      
+    page = payload.page
+    print(page)
+    
+    result = 'the resulto'
+    chat_history = []
+    
+    # Repack the payload
+    payload = {
+        "response": result,
+        "chat_history": chat_history,
+    }
+
+    return payload
+  
